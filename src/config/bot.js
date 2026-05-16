0400e77de1b1776da52554ff87de7ad10532aa9f
palumboaxel99-crypto <palumboaxel99@gmail.com>
2026-05-17 00:25:22 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "P.A.P GAME",
         // Activity type number (0 = Playing).
         type: 0, 
       },
